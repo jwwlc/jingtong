133eabe2b0dfe9d89b0d3805b9f47646402b07c8
wuhaotian0313 <2717312271@qq.com>
2018-09-29 21:43:52 +0800

    “wht_lianjie

--- js/drawback1.js
+++ js/drawback1.js
@@ -1,28 +1,28 @@
 window.onload=function () {
-    // let select=document.querySelector(".pleaseSelect .back-right");
+    let select=document.querySelector(".pleaseSelect .back-right");
     let buyworry=document.querySelector(".buyworry .back-right");
-    // let modal=document.querySelector(".modal");
+    let modal=document.querySelector(".modal");
     let ModalWorry=document.querySelector(".Modal-worry")
     let cross=document.querySelector(".modal .date-top span:nth-child(2)");
     let crosstwo=document.querySelector(".Modal-worry .date-top span:nth-child(2)");
-    // let btnClose1=document.querySelector(".btn-close1");
+    let btnClose1=document.querySelector(".btn-close1");
     let btnClose2=document.querySelector(".btn-close2");
-    console.log( buyworry,cross,crosstwo,ModalWorry,btnClose2);
-    // select.onclick=function () {
-    //     modal.style.display="block";
-    // }
-    // cross.onclick=function () {
-    //     modal.style.display="none";
-    // }
+    console.log(select, buyworry,cross,crosstwo,modal,ModalWorry,btnClose1,btnClose2);
+    select.onclick=function () {
+        modal.style.display="block";
+    }
+    cross.onclick=function () {
+        modal.style.display="none";
+    }
     buyworry.onclick=function () {
         ModalWorry.style.display="block";
     }
     crosstwo.onclick=function () {
         ModalWorry.style.display="none";
     }
-    // btnClose1.onclick=function () {
-    //     modal.style.display="none";
-    // }
+    btnClose1.onclick=function () {
+        modal.style.display="none";
+    }
     btnClose2.onclick=function () {
         modal.style.display="none";
     }
